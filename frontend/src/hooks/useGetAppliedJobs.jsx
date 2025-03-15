ed38a08d49579d60b7ef0651d8bc5881820a088d
sagar252004 <132927520+sagar252004@users.noreply.github.com>
2025-03-15 11:58:50 +0530

    adding long polling for real time updation

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -1,27 +1,37 @@
 import { setAllAppliedJobs } from "@/redux/jobSlice";
 import { APPLICATION_API_END_POINT } from "@/utils/constant";
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
+        let isPolling = true;
+
         const fetchAppliedJobs = async () => {
             try {
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
-                console.log(res.data);
-                if(res.data.success){
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, { withCredentials: true });
+                if (res.data.success) {
                     dispatch(setAllAppliedJobs(res.data.application));
                 }
             } catch (error) {
                 console.log(error);
             }
-        }
-        fetchAppliedJobs();
-    },[])
+        };
+
+        const pollAppliedJobs = async () => {
+            while (isPolling) {
+                await fetchAppliedJobs();
+                await new Promise(resolve => setTimeout(resolve, 5000)); // Wait 5 seconds before next poll
+            }
+        };
+
+        pollAppliedJobs();
+
+        return () => { isPolling = false; }; // Cleanup function to stop polling
+    }, [dispatch]);
 };
+
 export default useGetAppliedJobs;
